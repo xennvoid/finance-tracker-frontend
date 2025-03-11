d9fa3f6d5b6ba1579fa6ed88267dabef7d98c11a
xennvoid <thundvgod@gmail.com>
2025-03-11 23:02:06 +0200

    feat: add base routing with Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { CssBaseline } from '@mui/material';
+import Home from '@pages/Home';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </>
   );
 }
