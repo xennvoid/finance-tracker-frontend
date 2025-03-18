a55edb33836d850b3d9fb532afc6057bec0e27d6
xennvoid <thundvgod@gmail.com>
2025-03-18 14:01:55 +0200

    refactor: remove refresh token with localStorage logic

--- src/services/tokenServices.ts
+++ src/services/tokenServices.ts
@@ -1,5 +1,4 @@
 export const ACCESS_TOKEN_KEY = 'access_token';
-const REFRESH_TOKEN_KEY = 'refresh_token';
 
 export const getAccessToken = () => {
   return localStorage.getItem(ACCESS_TOKEN_KEY);
@@ -9,15 +8,6 @@ export const setAccessToken = (token: string) => {
   return localStorage.setItem(ACCESS_TOKEN_KEY, token);
 };
 
-export const getRefreshToken = () => {
-  return localStorage.getItem(REFRESH_TOKEN_KEY);
-};
-
-export const setRefreshToken = (token: string) => {
-  return localStorage.setItem(REFRESH_TOKEN_KEY, token);
-};
-
-export const clearTokens = () => {
+export const clearAccessToken = () => {
   localStorage.removeItem(ACCESS_TOKEN_KEY);
-  localStorage.removeItem(REFRESH_TOKEN_KEY);
 };
