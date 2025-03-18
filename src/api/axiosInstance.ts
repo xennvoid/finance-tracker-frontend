a4355fb6bccd09016287792e55e757c364a0a7ca
xennvoid <thundvgod@gmail.com>
2025-03-18 20:14:00 +0200

    feat: add axiosInstance request interceptor with access token header

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,3 +1,4 @@
+import { getAccessToken } from '@services/tokenServices';
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
@@ -6,3 +7,16 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
