54fe90229ef6f213da55ece515778431afb89cc4
xennvoid <thundvgod@gmail.com>
2025-03-18 20:23:56 +0200

    feat: add axiosInstance response interceptor with refresh token handling and repeated request

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
-import { getAccessToken } from '@services/tokenServices';
-import axios from 'axios';
+import { clearAccessToken, getAccessToken, setAccessToken } from '@services/tokenServices';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { AUTH_ENDPOINTS } from 'constants/apiEndpoints/auth';
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -18,5 +19,43 @@ axiosInstance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  },
+);
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  async (error) => {
+    if (
+      error.response?.status === 401 &&
+      error.response.data.error.errorCode === 'invalid_token' &&
+      !error.config._retry
+    ) {
+      try {
+        const originalRequest = error.config as CustomAxiosRequestConfig;
+
+        originalRequest._retry = true;
+
+        const response = await axios.get(BASE_URL + AUTH_ENDPOINTS.REFRESH_TOKEN, {
+          withCredentials: true,
+        });
+
+        if (response.status === 200) {
+          originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          setAccessToken(response.data.accessToken);
+          return axiosInstance(originalRequest);
+        }
+      } catch (error) {
+        console.error(error);
+        clearAccessToken();
+      }
+    }
+
+    return Promise.reject(error.response.data);
   },
 );
