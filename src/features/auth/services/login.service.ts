9045b04a17e17d8d21a40f1d0c329830e279b90c
xennvoid <thundvgod@gmail.com>
2025-03-21 17:54:22 +0200

    chore: rename axiosInstance.ts and update related imports

--- src/features/auth/services/login.service.ts
+++ src/features/auth/services/login.service.ts
@@ -1,4 +1,4 @@
-import { axiosInstance } from '@api/axiosInstance';
+import { axiosInstance } from '@api/axios-instance';
 import { ILoginFormData, ILoginResponse } from '@features/types/login.types';
 import { setAccessToken } from '@services/token.service';
 import { AUTH_ENDPOINTS } from '@constants/api-endpoints/auth';
