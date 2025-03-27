9ac47286af78eb19af6840003e9494235379d156
xennvoid <thundvgod@gmail.com>
2025-03-27 16:49:36 +0200

    feat: add register service

---
+++ src/features/auth/services/register.service.ts
@@ -0,0 +1,22 @@
+import { axiosInstance } from '@api/axios-instance';
+import { AUTH_ENDPOINTS } from '@constants/api-endpoints/auth';
+import { IRegisterResponse } from '../types/register.types';
+import { isFormFilled } from '../utils/is-form-filled';
+
+export const register = async (formData: FormData): Promise<void> => {
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const firstName = formData.get('firstName');
+  const lastName = formData.get('lastName');
+
+  if (!isFormFilled(formData)) {
+    throw new Error('All fields are required');
+  }
+
+  await axiosInstance.post<void, IRegisterResponse>(AUTH_ENDPOINTS.REGISTER, {
+    email,
+    password,
+    firstName,
+    lastName,
+  });
+};
