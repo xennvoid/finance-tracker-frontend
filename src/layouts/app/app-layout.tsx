274b10c594e2aaff4438a8c351d7e89edb30559d
xennvoid <thundvgod@gmail.com>
2025-03-31 21:58:44 +0300

    feat: add AppLayout component with header and sidemenu(Drawer)

---
+++ src/layouts/app/app-layout.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router';
+import Header from './components/header';
+import StyledDrawer from './components/styled-drawer';
+import Main from './components/main';
+import { useToggle } from '@hooks/useToogle';
+
+interface AppLayoutProps {}
+
+const DRAWER_WIDTH = 250;
+
+const AppLayout: FC<AppLayoutProps> = ({}) => {
+  const { opened, toggleOpened } = useToggle();
+
+  return (
+    <Box display="flex">
+      <Header open={opened} onClick={toggleOpened} drawerWidth={DRAWER_WIDTH} />
+      <StyledDrawer open={opened} onClick={toggleOpened} drawerWidth={DRAWER_WIDTH} />
+      <Main open={opened} drawerWidth={DRAWER_WIDTH}>
+        <Outlet />
+      </Main>
+    </Box>
+  );
+};
+
+export default AppLayout;
