3da8bd47f8cfcf72372cf927001e283b2c3fd7c5
xennvoid <thundvgod@gmail.com>
2025-04-03 21:31:18 +0300

    feat: add basic theme setup with custom palette

---
+++ src/theme.d.ts
@@ -0,0 +1,19 @@
+import '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    custom: {
+      titlePrimary: string;
+      titleSecondary: string;
+      textPrimary: string;
+    };
+  }
+
+  interface PaletteOptions {
+    custom?: {
+      titlePrimary: string;
+      titleSecondary: string;
+      textPrimary: string;
+    };
+  }
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    custom: { titlePrimary: '#343c6a', titleSecondary: '#718ebf', textPrimary: '#232323' },
+  },
+});
