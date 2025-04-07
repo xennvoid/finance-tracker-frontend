a5bb68d19e4354fad95667f3c616ae8bacbc0c3c
xennvoid <thundvgod@gmail.com>
2025-04-07 17:26:18 +0300

    fix: remove unfinished component from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,9 @@
-import PaymentCards from '@features/payment-card/components/payment-cards';
 import { FC } from 'react';
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = ({}) => {
-  return (
-    <>
-      <PaymentCards />
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
