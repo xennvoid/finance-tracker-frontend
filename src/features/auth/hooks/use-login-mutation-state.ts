eb87b096f036dc96882196a040bcf35e6634e028
xennvoid <thundvgod@gmail.com>
2025-04-07 19:16:12 +0300

    feat: add redirection to home page after successful login

--- src/features/auth/hooks/use-login-mutation-state.ts
+++ src/features/auth/hooks/use-login-mutation-state.ts
@@ -3,15 +3,19 @@ import { login } from '../services/login.service';
 import { useActionState } from 'react';
 import { ILoginFormData } from '../types/login.types';
 import { useCurrentUserContext } from '@contexts/current-user-context';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '@routes/routes';
 
 export const useLoginMutationState = () => {
   const { loginCurrentUser } = useCurrentUserContext();
+  const navigate = useNavigate();
 
   const { error, isError, isPending, mutate } = useMutation({
     mutationFn: login,
     onSuccess: (userData) => {
       const { email, firstName, lastName } = userData;
       loginCurrentUser({ email, firstName, lastName });
+      navigate(ROUTES.HOME);
     },
   });
 
