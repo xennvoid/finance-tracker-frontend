e3d1c1f241e00afe87f760189c94ef73a22fa8a8
xennvoid <thundvgod@gmail.com>
2025-04-09 22:39:17 +0300

    refactor: change the CustomCard props type to generic, so that it is possible to accept properties depending on the component prop

--- src/components/custom-card.tsx
+++ src/components/custom-card.tsx
@@ -1,16 +1,18 @@
-import { FC } from 'react';
+import { ElementType } from 'react';
 import { Card, CardProps } from '@mui/material';
 
-interface CustomCardProps extends CardProps {}
+type CustomCardProps<C extends ElementType> = CardProps<C, { component?: C }>;
 
-const CustomCard: FC<CustomCardProps> = ({ children, sx, ...props }) => {
+const CustomCard = <C extends ElementType = typeof Card>(props: CustomCardProps<C>) => {
+  const { children, sx, ...others } = props;
+
   return (
     <Card
       sx={{
         borderRadius: 6,
         ...sx,
       }}
-      {...props}>
+      {...others}>
       {children}
     </Card>
   );
