755aca9e2c77cf570fb2f30e0cf154030f93b8c4
xennvoid <thundvgod@gmail.com>
2025-04-10 15:35:35 +0300

    feat: add redirect to the login page on logout

--- src/pages/Logout/index.tsx
+++ src/pages/Logout/index.tsx
@@ -1,13 +1,17 @@
 import { useCurrentUserContext } from '@contexts/current-user-context';
+import { ROUTES } from '@routes/routes';
 import { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 interface LogoutPageProps {}
 
 const LogoutPage: FC<LogoutPageProps> = ({}) => {
   const { logOutCurrentUser } = useCurrentUserContext();
+  const navigate = useNavigate();
 
   useEffect(() => {
     logOutCurrentUser();
+    navigate(ROUTES.LOGIN);
   }, []);
 
   return <></>;
