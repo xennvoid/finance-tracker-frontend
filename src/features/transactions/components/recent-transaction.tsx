e1a704c8302fe670306179500cb017eac2087720
xennvoid <thundvgod@gmail.com>
2025-04-15 22:09:52 +0300

    feat: add RecentTransaction component

---
+++ src/features/transactions/components/recent-transaction.tsx
@@ -0,0 +1,38 @@
+import SpriteSvg from '@components/sprite-svg';
+import { Avatar, Box, colors, Typography } from '@mui/material';
+import { FC } from 'react';
+import { ITransaction } from '../types/transaction.types';
+import { formatDayMonthYear } from '@utils/format-day-month-year';
+import { formatCurrency } from '@utils/format-currency';
+
+interface RecentTransactionProps extends ITransaction {}
+
+const RecentTransaction: FC<RecentTransactionProps> = ({ text, type, amount, createdAt, card }) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2 }}>
+      <Avatar sx={{ backgroundColor: '#e7edff', width: 55, height: 55 }}>
+        <SpriteSvg
+          spritePath="/transaction-sprite"
+          id="transaction"
+          fill={type === 'income' ? colors.green[900] : colors.red[800]}
+        />
+      </Avatar>
+      <Box sx={{ flex: 1 }}>
+        <Typography sx={(theme) => ({ fontWeight: 500, color: theme.palette.custom.textPrimary })}>
+          {text}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={(theme) => ({ color: theme.palette.custom.titleSecondary })}>
+          {formatDayMonthYear(createdAt)}
+        </Typography>
+      </Box>
+      <Typography sx={{ fontWeight: 500, color: type === 'income' ? '#41d4a8' : '#ff4b4a' }}>
+        {type === 'income' ? '+' : '-'}
+        {formatCurrency(amount, card.currency)}
+      </Typography>
+    </Box>
+  );
+};
+
+export default RecentTransaction;
