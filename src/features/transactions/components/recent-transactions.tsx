ece42cd2fee2eba4bb7235ee67b5bb2bb0b5a39d
xennvoid <thundvgod@gmail.com>
2025-04-17 17:07:35 +0300

    feat: integrate isLoading state and placeholder in RecentTransactions component

--- src/features/transactions/components/recent-transactions.tsx
+++ src/features/transactions/components/recent-transactions.tsx
@@ -2,18 +2,44 @@ import CustomCard from '@components/custom-card';
 import { FC } from 'react';
 import { ITransaction } from '../types/transaction.types';
 import RecentTransaction from './recent-transaction';
+import RecentTransactionSkeleton from './recent-transaction-skeleton';
+import RecentTransactionsPlaceholder from './recent-transactions-placeholder';
 
 interface RecentTransactionsProps {
+  isLoading: boolean;
   transactions: ITransaction[];
+  transactionsAmount: number;
 }
 
-const RecentTransactions: FC<RecentTransactionsProps> = ({ transactions }) => {
+const RecentTransactions: FC<RecentTransactionsProps> = ({
+  transactions,
+  isLoading,
+  transactionsAmount,
+}) => {
+  let content = null;
+
+  if (isLoading) {
+    content = Array.from({ length: transactionsAmount }).map((_, idx) => (
+      <RecentTransactionSkeleton key={idx} />
+    ));
+  } else if (transactions.length > 0) {
+    content = transactions.map((transaction, idx) => (
+      <RecentTransaction key={idx} {...transaction} />
+    ));
+  } else {
+    content = <RecentTransactionsPlaceholder />;
+  }
+
   return (
     <CustomCard
-      sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 1.5, p: 3 }}>
-      {transactions.map((transaction, idx) => (
-        <RecentTransaction key={idx} {...transaction} />
-      ))}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        gap: 1.5,
+        p: 3,
+      }}>
+      {content}
     </CustomCard>
   );
 };
