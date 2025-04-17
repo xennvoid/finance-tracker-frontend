b2fd21851b1feeb2115c99f72f03cc636b842bcf
xennvoid <thundvgod@gmail.com>
2025-04-17 12:15:43 +0300

    refactor: simplify PaymentCards component layout

--- src/features/payment-card/components/payment-cards.tsx
+++ src/features/payment-card/components/payment-cards.tsx
@@ -3,6 +3,7 @@ import PaymentCardSkeleton from './payment-card/payment-card-skeleton';
 import PaymentCard from './payment-card';
 import AddPaymentCard from './payment-card/add-payment-card';
 import { ICard } from '../types/card.types';
+import { Box } from '@mui/material';
 
 interface PaymentCardsProps {
   isLoading: boolean;
@@ -11,26 +12,34 @@ interface PaymentCardsProps {
 }
 
 const PaymentCards: FC<PaymentCardsProps> = ({ isLoading, cards, cardsAmount }) => {
+  let content = null;
+
   if (isLoading) {
-    return (
-      <>
-        {Array.from({ length: cardsAmount }).map((_, idx) => (
-          <PaymentCardSkeleton key={idx} />
-        ))}
-      </>
-    );
+    content = Array.from({ length: cardsAmount }).map((_, idx) => (
+      <PaymentCardSkeleton key={idx} />
+    ));
+  } else {
+    content = [
+      ...cards.map((card, idx) => <PaymentCard key={idx} {...card} />),
+      ...Array.from({ length: cardsAmount - cards.length }).map((_, idx) => (
+        <AddPaymentCard key={cards.length + idx} />
+      )),
+    ];
   }
 
   return (
-    <>
-      {cards.map((card, idx) => (
-        <PaymentCard key={idx} {...card} />
-      ))}
-      {!isLoading &&
-        Array.from({ length: cardsAmount - cards.length }).map((_, idx) => (
-          <AddPaymentCard key={idx} />
-        ))}
-    </>
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: {
+          xs: 'repeat(auto-fit, minmax(170px, 1fr))',
+          lg: 'repeat(auto-fit, minmax(350px, 1fr))',
+        },
+        gap: 3.75,
+        gridAutoRows: '1fr',
+      }}>
+      {content}
+    </Box>
   );
 };
 
