3b866026d27209ea077064879073b097ecc3e9ea
xennvoid <thundvgod@gmail.com>
2025-04-21 18:23:51 +0300

    feat: add TransactionCellKey enum and array of enum keys

---
+++ src/features/transactions/enums/transaction-cell.enum.ts
@@ -0,0 +1,10 @@
+export enum TransactionCellKey {
+  DESCRIPTION = 'description',
+  TYPE = 'type',
+  NUMBER = 'number',
+  DATE = 'date',
+  AMOUNT = 'amount',
+  CURRENCY = 'currency',
+}
+
+export const TransactionCellKeys = Object.values(TransactionCellKey);
