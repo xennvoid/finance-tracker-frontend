566062d862c0f64fd63c52cff4915a87ae6154ac
xennvoid <thundvgod@gmail.com>
2025-04-24 21:17:50 +0300

    refactor: replace useGetCardsQuery hook and data extraction with useCardsData hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import CommonTitleHeader from '@components/common-title-header';
 import PaymentCards from '@features/payment-card/components/payment-cards';
-import { useGetCardsQuery } from '@features/payment-card/hooks/use-get-cards-query';
 import { Grid } from '@mui/material';
 import RecentTransactions from '@features/transactions/components/recent-transactions';
 import { ROUTES } from '@routes/routes';
 import { FC } from 'react';
 import Section from '@components/section';
 import { useRecentTransactionsData } from '@features/transactions/hooks/use-recent-transactions-data';
+import { useCardsData } from '@features/payment-card/hooks/use-cards-data';
 
 interface HomeProps {}
 
@@ -14,8 +14,7 @@ const CARDS_AMOUNT = 2;
 const TRANSACTIONS_AMOUNT = 3;
 
 const Home: FC<HomeProps> = ({}) => {
-  const { data, isLoading } = useGetCardsQuery(CARDS_AMOUNT);
-  const cards = data?.data || [];
+  const { data: cards, isLoading: isLoadingCards } = useCardsData(CARDS_AMOUNT);
 
   const { data: transactions, isLoading: isLoadingTransactions } =
     useRecentTransactionsData(TRANSACTIONS_AMOUNT);
@@ -24,7 +23,7 @@ const Home: FC<HomeProps> = ({}) => {
     <Grid container spacing={3}>
       <Section size={{ xs: 12, md: 6, lg: 8 }}>
         <CommonTitleHeader titleText="My Cards" linkText="See All" linkTo={ROUTES.CARDS} />
-        <PaymentCards isLoading={isLoading} cards={cards} cardsAmount={CARDS_AMOUNT} />
+        <PaymentCards isLoading={isLoadingCards} cards={cards} cardsAmount={CARDS_AMOUNT} />
       </Section>
       <Section size={{ xs: 12, md: 6, lg: 4 }}>
         <CommonTitleHeader titleText="Recent Transactions" />
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import CommonTitleHeader from '@components/common-title-header';
 import Section from '@components/section';
 import PaymentCards from '@features/payment-card/components/payment-cards';
-import { useGetCardsQuery } from '@features/payment-card/hooks/use-get-cards-query';
 import TransactionsTable from '@features/transactions/components/transactions-table';
 import { Grid } from '@mui/material';
 import { ROUTES } from '@routes/routes';
@@ -11,14 +10,14 @@ import { useTransactionsTableData } from '@features/transactions/hooks/use-trans
 import { useTransactionsTableFilter } from '@features/transactions/hooks/use-transactions-table-filter';
 import TransactionsTablePaginations from '@features/transactions/components/transactions-table/transactions-table-pagination';
 import { usePagination } from '@hooks/use-pagination';
+import { useCardsData } from '@features/payment-card/hooks/use-cards-data';
 
 interface TransactionsPageProps {}
 
 const CARDS_AMOUNT = 2;
 
 const TransactionsPage: FC<TransactionsPageProps> = ({}) => {
-  const { data, isLoading } = useGetCardsQuery(CARDS_AMOUNT);
-  const cards = data?.data || [];
+  const { data: cards, isLoading: isLoadingCards } = useCardsData(CARDS_AMOUNT);
 
   const { transactionsTypeFilter, handleTransactionsTypeFilterChange } =
     useTransactionsTableFilter();
@@ -31,7 +30,7 @@ const TransactionsPage: FC<TransactionsPageProps> = ({}) => {
     <Grid container spacing={3}>
       <Section size={{ xs: 12, md: 6, lg: 8 }}>
         <CommonTitleHeader titleText="My Cards" linkText="+ Add Card" linkTo={ROUTES.CARDS} />
-        <PaymentCards isLoading={isLoading} cards={cards} cardsAmount={CARDS_AMOUNT} />
+        <PaymentCards isLoading={isLoadingCards} cards={cards} cardsAmount={CARDS_AMOUNT} />
       </Section>
       <Section size={{ xs: 12, md: 6, lg: 4 }}>
         <CommonTitleHeader titleText="My expenses" />
