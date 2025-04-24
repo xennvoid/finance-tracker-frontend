7f221b082ad81ad9fe3c8fe2bb733338ed30f772
xennvoid <thundvgod@gmail.com>
2025-04-24 22:19:24 +0300

    refactor: extract Transactions page transactions table section layout and API layer into separate component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,25 +1,13 @@
 import { FC } from 'react';
 import CommonTitleHeader from '@components/common-title-header';
 import Section from '@components/section';
-import TransactionsTable from '@features/transactions/components/transactions-table';
 import { Grid } from '@mui/material';
-import TransactionsTableTabs from '@features/transactions/components/transactions-table/transactions-table-tabs';
-import { useTransactionsTableData } from '@features/transactions/hooks/use-transactions-table-data';
-import { useTransactionsTableFilter } from '@features/transactions/hooks/use-transactions-table-filter';
-import TransactionsTablePaginations from '@features/transactions/components/transactions-table/transactions-table-pagination';
-import { usePagination } from '@hooks/use-pagination';
 import CardsSection from './sections/cards-section';
+import TransactionsTableSection from './sections/transactions-table-section';
 
 interface TransactionsPageProps {}
 
 const TransactionsPage: FC<TransactionsPageProps> = ({}) => {
-  const { transactionsTypeFilter, handleTransactionsTypeFilterChange } =
-    useTransactionsTableFilter();
-
-  const { page, setPage, handlePageChange } = usePagination();
-
-  const { transactions, pagination } = useTransactionsTableData(transactionsTypeFilter, page);
-
   return (
     <Grid container spacing={3}>
       <Section size={{ xs: 12, md: 6, lg: 8 }}>
@@ -29,22 +17,7 @@ const TransactionsPage: FC<TransactionsPageProps> = ({}) => {
         <CommonTitleHeader titleText="My expenses" />
       </Section>
       <Section size={12} sx={{ '& > :nth-of-type(2)': { mb: 3 } }}>
-        <CommonTitleHeader titleText="Transactions" />
-        <TransactionsTableTabs
-          filterValue={transactionsTypeFilter}
-          handleFilterChange={(e, v) => {
-            handleTransactionsTypeFilterChange(e, v);
-            setPage(1);
-          }}
-        />
-        <TransactionsTable transactions={transactions} />
-        {pagination && (
-          <TransactionsTablePaginations
-            page={pagination.page}
-            count={pagination.totalPages}
-            onChange={handlePageChange}
-          />
-        )}
+        <TransactionsTableSection />
       </Section>
     </Grid>
   );
---
+++ src/pages/Transactions/sections/transactions-table-section.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import CommonTitleHeader from '@components/common-title-header';
+import TransactionsTable from '@features/transactions/components/transactions-table';
+import TransactionsTablePaginations from '@features/transactions/components/transactions-table/transactions-table-pagination';
+import TransactionsTableTabs from '@features/transactions/components/transactions-table/transactions-table-tabs';
+import { useTransactionsTableData } from '@features/transactions/hooks/use-transactions-table-data';
+import { useTransactionsTableFilter } from '@features/transactions/hooks/use-transactions-table-filter';
+import { usePagination } from '@hooks/use-pagination';
+
+interface TransactionsTableSectionProps {}
+
+const TransactionsTableSection: FC<TransactionsTableSectionProps> = ({}) => {
+  const { transactionsTypeFilter, handleTransactionsTypeFilterChange } =
+    useTransactionsTableFilter();
+
+  const { page, setPage, handlePageChange } = usePagination();
+
+  const { transactions, pagination } = useTransactionsTableData(transactionsTypeFilter, page);
+
+  return (
+    <>
+      <CommonTitleHeader titleText="Transactions" />
+      <TransactionsTableTabs
+        filterValue={transactionsTypeFilter}
+        handleFilterChange={(e, v) => {
+          handleTransactionsTypeFilterChange(e, v);
+          setPage(1);
+        }}
+      />
+      <TransactionsTable transactions={transactions} />
+      {pagination && (
+        <TransactionsTablePaginations
+          page={pagination.page}
+          count={pagination.totalPages}
+          onChange={handlePageChange}
+        />
+      )}
+    </>
+  );
+};
+
+export default TransactionsTableSection;
