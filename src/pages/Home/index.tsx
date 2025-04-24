b3f3787f9c1b06d8194da1e28c87c3bc5c988b99
xennvoid <thundvgod@gmail.com>
2025-04-24 22:06:45 +0300

    refactor: extract Home page recent transactions section layout and API layer into separate component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,19 @@
-import CommonTitleHeader from '@components/common-title-header';
 import { Grid } from '@mui/material';
-import RecentTransactions from '@features/transactions/components/recent-transactions';
 import { FC } from 'react';
 import Section from '@components/section';
-import { useRecentTransactionsData } from '@features/transactions/hooks/use-recent-transactions-data';
 import CardsSection from './sections/cards-section';
+import RecentTransactionsSection from './sections/recent-transactions-section';
 
 interface HomeProps {}
 
-const TRANSACTIONS_AMOUNT = 3;
-
 const Home: FC<HomeProps> = ({}) => {
-  const { data: transactions, isLoading: isLoadingTransactions } =
-    useRecentTransactionsData(TRANSACTIONS_AMOUNT);
-
   return (
     <Grid container spacing={3}>
       <Section size={{ xs: 12, md: 6, lg: 8 }}>
         <CardsSection />
       </Section>
       <Section size={{ xs: 12, md: 6, lg: 4 }}>
-        <CommonTitleHeader titleText="Recent Transactions" />
-        <RecentTransactions
-          transactions={transactions}
-          isLoading={isLoadingTransactions}
-          transactionsAmount={TRANSACTIONS_AMOUNT}
-        />
+        <RecentTransactionsSection />
       </Section>
     </Grid>
   );
