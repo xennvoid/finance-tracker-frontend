971b99f75f9673223d1703ef5eebeb84c1b36628
xennvoid <thundvgod@gmail.com>
2025-05-31 20:49:28 +0300

    feat: add hook for monthly summary chart data

---
+++ src/features/charts/hooks/use-monthly-summary-chart-data.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { getMonthlySummary } from '@features/transactions/services/get-monthly-summary.service';
+import { IMonthlySummaryChartData } from '../types/monthly-summary-chart.types';
+
+export const useMonthlySummaryChartData = ({
+  previousMonthsAmount,
+  cardId,
+}: {
+  previousMonthsAmount?: number;
+  cardId: string;
+}) => {
+  const { data: summaryChartData = [] } = useQuery({
+    queryKey: ['monthly-summary', previousMonthsAmount, cardId],
+    enabled: !!cardId,
+    queryFn: () => getMonthlySummary({ previousMonthsAmount, cardId }),
+    select: (data): IMonthlySummaryChartData => {
+      const { income, expense } = data[0];
+      return [
+        { name: 'Incomes', value: income },
+        { name: 'Expenses', value: expense },
+      ].filter((summary) => summary.value > 0);
+    },
+  });
+
+  return { summaryChartData };
+};
