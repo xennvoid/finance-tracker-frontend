c36333fb35301660a2763cbc487e03a57b0c4c5a
xennvoid <thundvgod@gmail.com>
2025-06-16 22:28:16 +0300

    feat: add CardListSection with context, pagination and api integration

---
+++ src/pages/Cards/sections/card-list-section.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import CommonTitleHeader from '@components/common-title-header';
+import SectionContent from '@components/section-content';
+import PaymentCardsPreviewList from '@features/payment-card/components/payment-card-preview/payment-card-preview-list';
+import { usePagination } from '@hooks/use-pagination';
+import { useCardsData } from '@features/payment-card/hooks/use-cards-data';
+import CustomPagination from '@components/custom-pagination';
+import { CardListPaginationProvider } from '@features/payment-card/context/card-list-pagination-context';
+
+interface CardListSectionProps {}
+
+const CardListSection: FC<CardListSectionProps> = ({}) => {
+  const { page, setPage, handlePageChange } = usePagination();
+
+  const { data, pagination } = useCardsData({ page });
+
+  return (
+    <>
+      <CommonTitleHeader titleText="Card List" />
+      <SectionContent>
+        <CardListPaginationProvider setPage={setPage}>
+          <PaymentCardsPreviewList data={data} />
+        </CardListPaginationProvider>
+        {pagination && (
+          <CustomPagination page={page} count={pagination.totalPages} onChange={handlePageChange} />
+        )}
+      </SectionContent>
+    </>
+  );
+};
+
+export default CardListSection;
