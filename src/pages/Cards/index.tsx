ebe14e8e6c6d1bbd2b57c2dc569f3f70c36e3197
xennvoid <thundvgod@gmail.com>
2025-06-17 17:52:46 +0300

    feat: integrate card list section into cards page

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react';
+import { Grid } from '@mui/material';
+import CardListSection from './sections/card-list-section';
+import Section from '@components/section';
 
 interface CardsPageProps {}
 
 const CardsPage: FC<CardsPageProps> = ({}) => {
-  return <></>;
+  return (
+    <Grid container spacing={3}>
+      <Section size={{ xs: 12, md: 6, lg: 8 }}>
+        <CardListSection />
+      </Section>
+    </Grid>
+  );
 };
 
 export default CardsPage;
