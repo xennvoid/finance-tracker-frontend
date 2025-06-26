b38ea06bdb7ed570fb0cc8bf55ff7a30d579bd38
xennvoid <thundvgod@gmail.com>
2025-06-26 19:53:01 +0300

    feat: add ActiveCardSection to the cards page

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import CardListSection from './sections/card-list-section';
 import Section from '@components/section';
 import AddCardSection from './sections/add-card-section';
+import ActiveCardSection from './sections/active-card-section';
 
 interface CardsPageProps {}
 
 const CardsPage: FC<CardsPageProps> = ({}) => {
   return (
     <Grid container spacing={3}>
+      <Section
+        size={{ xs: 12, md: 6, lg: 4 }}
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Box>
+          <ActiveCardSection />
+        </Box>
+      </Section>
       <Section size={{ xs: 12, md: 6, lg: 8 }}>
         <CardListSection />
       </Section>
