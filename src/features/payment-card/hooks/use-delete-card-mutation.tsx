8c78783594b448f8faac8a518e43510383e904b1
xennvoid <thundvgod@gmail.com>
2025-06-29 13:16:02 +0300

    feat: add active card reset after confirming delete modal

--- src/features/payment-card/hooks/use-delete-card-mutation.tsx
+++ src/features/payment-card/hooks/use-delete-card-mutation.tsx
@@ -1,8 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCard } from '../services/delete-card.service';
 import { toast } from 'react-toastify';
+import { ICard } from '../types/card.types';
 
-export const useDeleteCardMutation = (setPage: React.Dispatch<React.SetStateAction<number>>) => {
+export const useDeleteCardMutation = (
+  setPage: React.Dispatch<React.SetStateAction<number>>,
+  setActiveCard: React.Dispatch<React.SetStateAction<ICard | null>>,
+) => {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -11,6 +15,7 @@ export const useDeleteCardMutation = (setPage: React.Dispatch<React.SetStateActi
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['cards'] });
       setPage(1);
+      setActiveCard(null);
       toast.success('Card deleted successfully');
     },
   });
